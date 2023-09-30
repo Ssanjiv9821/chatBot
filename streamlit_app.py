c3a72c2a23dbce9d6ed27a5934adbe122c7364cd
Ssanjiv9821 <86623937+Ssanjiv9821@users.noreply.github.com>
2023-09-30 02:14:09 +0000

    3rd committ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 
 import streamlit as st
-
+import os
 """
 # Open Ai Based ChatBot
 
 """
+os.environ['OPENAI_API_KEY'] = st.secrets["chatbot-asnnwii3pco2jv6wtzslqw"]
+
+st.write(os.environ['OPENAI_API_KEY'])
 st.write("Please type a question regarding the vision pro headet lets see if this updates!!")
 st.text_input("Question", key="question")
 question = st.session_state.question
