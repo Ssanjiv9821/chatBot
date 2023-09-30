d08eb6503ab0635f8624ee2f0e7e1b0d0438e5de
Ssanjiv9821 <86623937+Ssanjiv9821@users.noreply.github.com>
2023-09-30 02:08:18 +0000

    2nd committ

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Open Ai Based ChatBot
 
 """
-st.write("Please type a question regarding the vision pro headset!!")
+st.write("Please type a question regarding the vision pro headet lets see if this updates!!")
 st.text_input("Question", key="question")
 question = st.session_state.question
 
